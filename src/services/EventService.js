75f18e28e7f6fac475ebb10e1eb101f19c6f56bb
Nattaparin <nattaparin455@gmail.com>
2022-08-04 00:21:46 +0700

    5.3

--- src/services/EventService.js
+++ src/services/EventService.js
@@ -17,6 +17,9 @@ export default {
   getEvent(id) {
     return apiClient.get('/passenger/' + id)
   },
+  getEventAir(id) {
+    return apiClient.get('/airline/' + id)
+  },
   getEventsAir() {
     return apiClient.get('/airline')
   },
